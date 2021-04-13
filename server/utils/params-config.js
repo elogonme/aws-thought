6233084581df5f699c7d964e9cd0264f68093bb5
Eldar <elogon@gmail.com>
2021-04-12 23:34:15 -0400

    Integrate API upload images to S3

---
+++ server/utils/params-config.js
@@ -0,0 +1,14 @@
+const { v4: uuidv4 } = require("uuid");
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split(".");
+  const fileType = myFile[myFile.length - 1];
+  const imageParams = {
+    Bucket: "user-images-5c1d4f29-aebb-41e6-975e-611b0621c26c",
+    Key: `${myFile[0]}-${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+  return imageParams;
+};
+
+module.exports = params;
